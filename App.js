97b1172cf9afcd21c55d27be103f018ea07a30ac
rtodd001 <rtodd001@ucr.edu>
2020-04-14 00:05:37 -0700

    Button push now send the request instead of getting the request on boot

--- App.js
+++ App.js
@@ -9,59 +9,81 @@ class App extends Component{
       error : null,
       isLoaded: false,
       mess: "Press to connect",
+      click: false,
       items: []
     }
+    this.onPress = this.onPress.bind(this)
   }
 
   componentDidMount(){
-    this.fetchData()
+    this.setState({
+      ... this.state,
+      isLoaded: true
+    })
   }
 
-  fetchData(){
+  componentDidUpdate(prevProps, prevState){
+    //const { isLoaded, click} = this.state
     //For mobile, you have to put your own IP instead of using local host
     //you can find the correct IP in the bottom lef of the metro bundler by
     //the QR code. Replace <IP> below with yours
     //fetch("http://<IP>:5000/")
-    fetch("http://localhost:5000/")
-    .then(response => response.json())
-    .then(
-      (result) => {
-        this.setState({
-          isLoaded: true,
-          items: result.items
-        });
-      },
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        })
-      }
-    )
+    if(prevState.click !== this.state.click){
+      console.log("Fetching")
+      fetch("http://localhost:5000/")
+      .then(response => response.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.item,
+            mess: result.item[0].data
+          });
+        },
+        (error) => {
+          this.setState({
+            error
+          })        
+      }) 
+    }
   }
 
   onPress = () => {
     this.setState({
-      mess : this.state.items[0].data
+      ...this.state,
+      click: true
+      //mess: this.state.items[0].data
     })
+    console.log("In press")
   }
 
   render(){
   
     console.log(this.state)
-    const { error, isLoaded, items } = this.state;
+    const { error, isLoaded, click } = this.state;
     //help
 
     if (error) {
       return <View><Text>{error.message}</Text></View>
-    } else if (!isLoaded) {
+    } 
+    else if (!isLoaded) {
       return <View><Text>Loading...</Text></View>
-    } return (
-      <View style={styles.container}>
-        <Text>{this.state.mess}</Text>
-        <Button title ="Press Me!" onPress={() => this.onPress()}/>
-      </View>
-    );
+    } 
+    else if (!click) {
+      return (
+        <View style={styles.container}>
+          <Button title ="Press Me!" onPress={() => this.onPress()}/>
+        </View>
+      );
+    }
+    else{
+      return(
+        <View style={styles.container}>
+          <Text>{this.state.mess}</Text>
+        </View>
+        
+      )
+    }
   }
 }
   const styles = StyleSheet.create({
--- express.js
+++ express.js
@@ -5,6 +5,9 @@ const port = 5000
 
 //app.get('/', (req, res) => res.send('Hello World!'))
 app.use(cors())
-app.get('/', (req, res) => res.status(200).json({"items":[{"data": "Connection!"}]}));
+app.get('/', (req, res) => {
+    res.status(200).json({"item":[{"data": "Connection!"}]})
+    console.log(`Request received`)
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
