1e026d71e2a2def7d2e061c3a3b499024135dd45
rtodd001 <rtodd001@ucr.edu>
2020-04-19 00:01:52 -0700

    Able to read from the csv file uploaded to github

--- src/express.js
+++ src/express.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const read = require('./read')
 const cors = require('cors')
 const app = express()
 const port = 5000
 
 //app.get('/', (req, res) => res.send('Hello World!'))
+var temp = read.getCSV()
 app.use(cors())
 app.get('/', (req, res) => {
-    res.status(200).json({"item":[{"data": "Connection!"}]})
-    console.log(`Request received`)
+    res.status(200).json({"item":[{"data": temp}]})
+    console.log(temp)
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- src/read.js
+++ src/read.js
@@ -1,5 +1,12 @@
-export default() =>{
-    
+//import axios from 'axios';
+const axios = require('axios')
 
-
-}
+const getCSV = () => {
+    var str = [];
+    var url = 'https://raw.githubusercontent.com/rtodd001/ttteam/rtodd/data/dummy.csv'
+    axios.get(url).then(res => {
+        str.push(res.data);
+    })
+    return str;
+};
+exports.getCSV = getCSV;
