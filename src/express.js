2548c70ed24ee5cb3655dcd5e4e1cf8b5cf4fac3
rtodd001 <rtodd001@ucr.edu>
2020-04-19 20:11:41 -0700

    Able to succesfully read the csv file

--- src/express.js
+++ src/express.js
@@ -5,11 +5,17 @@ const app = express()
 const port = 5000
 
 //app.get('/', (req, res) => res.send('Hello World!'))
-var temp = read.getCSV()
+var strsplit = read.getCSV()
+
 app.use(cors())
 app.get('/', (req, res) => {
-    res.status(200).json({"item":[{"data": temp}]})
-    console.log(temp)
+    //there are commas in the names of products used so we cannot use split()
+    //this regex replaces split() and will extract all the cells that we need into
+    //an array of strings
+    let re =/(\"+(?=.*\").*\")|(^\"\?|\/|\.|\;|\·|\-|\:|\w|\s$)+/g
+    var lst = strsplit[0].toString().match(re)
+    res.status(200).json({"item":[{"data": lst}]})
+    console.log(lst)
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
