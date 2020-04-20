29ecde0b3e9a98a8abed58629b06a1fda0d86f1d
Sam Tapia <stapi010@ucr.edu>
2020-04-19 22:55:53 -0700

    created scr directory HomeScreen.js and added team logo splash screen.

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const HomeScreen = () => {
+    return <View>
+        <Text>This is the HomeScreen!</Text>
+    </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
