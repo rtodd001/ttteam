07fde68e547e165f0f22379b00f30d109b315680
Sam Tapia <stapi010@ucr.edu>
2020-05-16 13:03:23 -0700

    homescreen flex

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import {Button, Text, View, StyleSheet, Image, Dimensions} from 'react-native';
+import { Button, useState, Text, View, StyleSheet, Image, Dimensions } from 'react-native';
 
-const HomeScreen = ({navigation}) => {
-    return <View>
-        <Image style={{resizeMode: 'center', width:Dimensions.get('window').width, height:200}}source={require('../../assets/ttteamLogo.jpg')}/>
+const HomeScreen = ({ navigation }) => {
+    console.log(Dimensions.get('window').width);
+    return <View style={styles.container}>
+
+        <Image style={styles.logo}
+            source={require('../../assets/ttteamLogo.jpg')}
+        />
+
+        <View style={{ paddingHorizontal: 20, paddingVertical: 5 }}>
+            <Button style={styles.button}
+                title="Search"
+                onPress={() => navigation.navigate('Search')}
+            />
+        </View>
+
+        <View style={{ paddingHorizontal: 20, paddingVertical: 5 }}>
+            <Button style={styles.button}
+                title="Analysis"
+                onPress={() => navigation.navigate('Analysis')}
+            />
+        </View>
 
 
     </View>
@@ -27,5 +45,4 @@ const styles = StyleSheet.create({
         maxWidth: 150
     }
 });
-
 export default HomeScreen;
