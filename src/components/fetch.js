a67a7b96543bc6db345815dca181c5a5e5280f7c
sha021 <43655180+sha021@users.noreply.github.com>
2020-05-18 00:34:02 -0700

    columns deleted for Analysis:topCountries fetch

--- src/components/fetch.js
+++ src/components/fetch.js
@@ -242,7 +242,7 @@ async function popCat(ID, name, category, mainCategory, state, backers, country,
     return array[0]; 
 }
 
-async function topCountries(ID, name, category, mainCategory, currency, deadline, goal, launched, pledged, state, backers, country, usdPledged, usdPledgedReal, usdGoalReal) {
+async function topCountries(ID, name, category, mainCategory, state, backers, country, usdPledgedReal, usdGoalReal) {
     // var url = new URL('http://192.168.1.8:5000/search')
     var url = new URL('http://localhost:5000/analysis/topCountries')
     var params = {
@@ -250,15 +250,9 @@ async function topCountries(ID, name, category, mainCategory, currency, deadline
         'name' : name,
         'category' : category,
         'main_category': mainCategory,
-        'currency' : currency,
-        'deadline' : deadline,
-        'goal' : goal,
-        'launched' : launched,
-        'pledged' : pledged,
         'state' : state,
         'backers' : backers,
         'country' : country,
-        'usd pledged' : usdPledged,
         'usd_pledged_real' : usdPledgedReal,
         'usd_goal_real' : usdGoalReal
     } 
